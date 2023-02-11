a17ac970b6940bdad8facace73250c19ed4f6696
Ege Aydemir <egeaydemirr97@gmail.com>
2023-02-11 19:34:40 +0300

    Initial Commit

---
+++ Firebase Kayit Olma ve Giris Auth/Firebase_Kayit_Olma_ve_Giris_AuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_Kayit_Olma_ve_Giris_AuthApp.swift
+//  Firebase Kayit Olma ve Giris Auth
+//
+//  Created by Ege Aydemir on 11.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_Kayit_Olma_ve_Giris_AuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
